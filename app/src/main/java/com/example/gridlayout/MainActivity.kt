9bcc298477fb9b9f52312d276b98e49ec9efe457
Fer2345Romero <fernandaromeroclaros@gmail.com>
2025-09-19 12:01:23 -0600

    cambios

--- app/src/main/java/com/example/gridlayout/MainActivity.kt
+++ app/src/main/java/com/example/gridlayout/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.gridlayout
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import lenguajes
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var card:CardView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +20,15 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        card = findViewById(R.id.lenguajes)
+
+        card.setOnClickListener{
+            val intent = Intent(
+                this,
+                lenguajes::class.java)
+            startActivity( intent)
+
         }
     }
 }
